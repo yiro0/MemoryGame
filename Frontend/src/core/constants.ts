e327c261fc1791ded51c6b2377d24eb0aaedb9a2
yiro0 <bpalucki@icloud.com>
2026-06-30 15:21:21 +0200

    Changed amounts of pairs per difficulties from odd to even

--- Frontend/src/core/constants.ts
+++ Frontend/src/core/constants.ts
@@ -1,7 +1,7 @@
 export const DIFFICULTIES = {
-    easy: {pairs: 5, label: "Easy "},
-    medium: {pairs: 9, label: "Medium "},
-    hard: {pairs: 14, label: "Hard "},
+    easy: {pairs: 6, label: "Easy "},
+    medium: {pairs: 8, label: "Medium "},
+    hard: {pairs: 10, label: "Hard "},
 } as const;
 
 export type Difficulty = keyof typeof DIFFICULTIES;
